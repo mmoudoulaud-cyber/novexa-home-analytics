e71bd50395f6ab67380ef81bcec88f85466ba51e
Merryl Moudoulaud <mmoudoulaud@gmail.com>
2026-07-13 15:32:07 +0200

    feat(date): generate date dimension

---
+++ python/test_date_generator.py
@@ -0,0 +1,13 @@
+from generators.date_generator import (
+    generate_date_dimension,
+    validate_date_dimension,
+    export_date_dimension,
+)
+
+df = generate_date_dimension()
+
+validate_date_dimension(df)
+
+export_date_dimension(df)
+
+print(df.head())
